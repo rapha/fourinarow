de96967107784f32669915afd45454dc0b2d021a
Raphael Speyer <rspeyer@gmail.com>
2009-11-05 18:25:41 +1100

    added visual indicator of current player

--- gui.ml
+++ gui.ml
@@ -5,26 +5,37 @@ open Player
 open Util
 
 let _ =
+  let game = ref new_game in
+  let drop col _ = (game := play_turn (fun _ -> col) !game; ()) in
+
   let widget = openTk () in
 
-  let on_drop = function
-    | Drop (row, col, player) -> 
-        let colour = match player with A -> `Yellow | B -> `Red in
-        grid ~row:(7-row) ~column:(col-1) [Label.create ~text:"    " ~background:colour widget]
-    | _ -> () 
+  let colour player = match player with A -> `Yellow | B -> `Red in
 
-  and on_win = function
+  let piece player =
+    Label.create ~text:"    " ~background:(colour player) widget in
+
+  let add_button_row bgcolour =
+    foldi (fun _ col -> grid ~column:col ~row:0 [Button.create ~text:"v" ~highlightbackground:bgcolour ~command:(drop (col+1)) widget]) () 7 in
+
+  let on_drop = function
+    | Drop (row, col, player) -> grid ~row:(7-row) ~column:(col-1) [piece player]
+    | _ -> () in
+
+  let on_switch = function
+    | Switch player -> add_button_row (colour player)
+    | _ -> () in
+
+  let on_win = function
     | Win player -> 
         let message = (string_of_player player) ^ " wins" in 
-        Dialog.create ~parent:widget ~title:"Game Over" ~message:message ~buttons:["OK"] () |> ignore;
+        Dialog.create ~parent:widget ~title:"Game Over" ~message:message ~buttons:["OK"] ~default:0 () |> ignore;
         closeTk ()
     | _ -> () in
 
-  let game = new_game |> handle on_drop |> handle on_win |> ref in
+  game := !game |> handle on_drop |> handle on_win |> handle on_switch;
 
-  let drop col _ = (game := play_turn (fun _ -> col) !game; ()) in
-
-  foldi (fun _ col -> grid ~column:col ~row:0 [Button.create ~text:"v" ~command:(drop (col+1)) widget]) () 7;
+  add_button_row `Black;
   foldi (fun _ row ->
     foldi (fun _ col -> 
       grid ~column:col ~row:(row+1) [Label.create ~text:"." ~borderwidth:4 widget]
