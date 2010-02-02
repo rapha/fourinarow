9275ee378bb97dbdfe99a971b16874c376853f85
Raphael Speyer <rspeyer@gmail.com>
2010-02-02 16:05:51 -0500

    tidied a bit and added test for choose_move

--- test.ml
+++ test.ml
@@ -6,25 +6,25 @@ let _ =
   let move _ = 3 in
 
   run_test_tt ("Player" >::: [
-      "to_string A" >:: (fun() -> 
+      "to_string A" >:: (fun() ->
         Player.A |> Player.to_string |> assert_equal "A"
       );
-      "to_string B" >:: (fun() -> 
+      "to_string B" >:: (fun() ->
         Player.B |> Player.to_string |> assert_equal "B"
       );
   ]) |> ignore;
   run_test_tt ("Board.wins" >::: [
-      "false for empty" >:: (fun() -> 
+      "false for empty" >:: (fun() ->
         Board.empty |> Board.wins Player.A |> assert_equal false
       );
-      "false for vertical line of 3" >:: (fun() -> 
+      "false for vertical line of 3" >:: (fun() ->
         Board.build [
           "A------";
           "A------";
           "A------"]
           |> Board.wins Player.A |> assert_equal false
       );
-      "true for vertical line of 4" >:: (fun() -> 
+      "true for vertical line of 4" >:: (fun() ->
         Board.build [
           "A------";
           "A------";
@@ -32,12 +32,12 @@ let _ =
           "A------"]
         |> Board.wins Player.A |> assert_equal true
       );
-      "false for horizontal line of 3" >:: (fun() -> 
+      "false for horizontal line of 3" >:: (fun() ->
         Board.build [
-          "AAA----"] 
+          "AAA----"]
         |> Board.wins Player.A |> assert_equal false
       );
-      "true for horizontal line of 4" >:: (fun() -> 
+      "true for horizontal line of 4" >:: (fun() ->
         Board.build [
           "AAAA---"]
         |> Board.wins Player.A |> assert_equal true
@@ -91,11 +91,11 @@ let _ =
           "-------\n" )
       );
   ]) |> ignore;
-  
+
   run_test_tt ("game" >::: [
     "uses argument passed in to get row to drop in" >:: (fun() ->
       let col = ref None in
-      let module TestGame = Game.Make (struct include Board 
+      let module TestGame = Game.Make (struct include Board
         let drop _ c board =
           col := Some c;
           board
@@ -123,9 +123,9 @@ let _ =
     );
     "play_turn calls win handler" >:: (fun() ->
       let first _ = 1 and second _ = 2 in
-      let game = TestGame.create Player.A Player.B |> 
-        TestGame.play_turn first |> TestGame.play_turn second |> 
-        TestGame.play_turn first |> TestGame.play_turn second |> 
+      let game = TestGame.create Player.A Player.B |>
+        TestGame.play_turn first |> TestGame.play_turn second |>
+        TestGame.play_turn first |> TestGame.play_turn second |>
         TestGame.play_turn first |> TestGame.play_turn second in
       let win_handled = ref false in
       let handler = function TestGame.Win Player.A -> win_handled := true | _ -> () in
@@ -143,21 +143,21 @@ let _ =
   ]) |> ignore;
 
   run_test_tt ("AI" >::: [
-      "minimax returns all 0 if empty" >:: (fun() -> 
+      "minimax returns all 0 if empty" >:: (fun() ->
         let module TestAI = Ai.Make(Board) in
-        Board.empty |> TestAI.minimax Player.A 0 (Player.A,Player.B) |> List.of_enum |> 
+        Board.empty |> TestAI.minimax 0 Player.A (Player.A,Player.B) |> List.of_enum |>
         assert_equal (List.make 7 0.)
       );
-      "minimax returns all -Inf if opponent has won" >:: (fun() -> 
-        let module TestAI = Ai.Make (struct include Board 
+      "minimax returns all -Inf if opponent has won" >:: (fun() ->
+        let module TestAI = Ai.Make (struct include Board
           let wins player board =
             player = Player.B
         end) in
-        TestAI.minimax Player.A 0 (Player.A,Player.B) Board.empty |> List.of_enum |> 
+        TestAI.minimax 0 Player.A (Player.A,Player.B) Board.empty |> List.of_enum |>
         assert_equal (List.make 7 neg_infinity)
       );
-      "minimax with depth 1 returns Inf if column makes player win" >:: (fun() -> 
-        let module TestAI = Ai.Make (struct include Board 
+      "minimax with depth 1 returns Inf if column makes player win" >:: (fun() ->
+        let module TestAI = Ai.Make (struct include Board
           let last_drop = ref None
           let drop _ column board =
             last_drop := Some column;
@@ -165,42 +165,51 @@ let _ =
           let wins player board =
             !last_drop = Some 5
         end) in
-        TestAI.minimax Player.A 1 (Player.A, Player.B) Board.empty |> List.of_enum |>
+        TestAI.minimax 1 Player.A (Player.A, Player.B) Board.empty |> List.of_enum |>
         assert_equal [0.; 0.; 0.; 0.; infinity; 0.; 0.]
         );
-      "minimax with depth 0 returns values from eval function" >:: (fun() -> 
+      "minimax with depth 0 returns values from eval function" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
-          let evaluate player board = 
+          let evaluate player board =
             match (1 -- 7) |> map (flip top_row board) |> List.of_enum with
             | [1; 0; 0; 0; 0; 0; 0] ->  5.
             | [0; 1; 0; 0; 0; 0; 0] ->  8.
             | [0; 0; 1; 0; 0; 0; 0] -> -6.
             | [0; 0; 0; 1; 0; 0; 0] ->  1.
-            | _ -> 0. 
+            | _                     ->  0.
         end) in
 
-        TestAI.minimax Player.A 0 (Player.A, Player.B) Board.empty |> List.of_enum |>
+        TestAI.minimax 0 Player.A (Player.A, Player.B) Board.empty |> List.of_enum |>
         assert_equal [5.; 8.; -6.; 1.; 0.; 0.; 0.]
         );
-      "minimax with depth 1 returns lowest eval values from next level" >:: (fun() -> 
+      "minimax with depth 1 returns lowest eval values from next level" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
-          let evaluate player board = 
-            if board = build ["AB-----";] then  -1. else
-            if board = build ["A-B----";] then   0. else
-            if board = build ["A--B---";] then   1. else
-
-            if board = build ["BA-----";] then   0. else
-            if board = build ["-AB----";] then  -3. else
-
-            if board = build ["B-A----";] then -10. else
-
-            if board = build ["B--A---";] then  -4. else
-            if board = build ["-B-A---";] then  -1. else
-            if board = build ["--BA---";] then   3. else
-                                                 0. 
+          let evaluate player board =
+            match board |> to_string |> Str.split (Str.regexp "\n") |> List.last with
+            | "AB-----" ->  -2.
+            | "A-B----" ->  -1.
+            | "A--B---" ->   1.
+            | "BA-----" ->   4.
+            | "-AB----" ->  -3.
+            | "B-A----" -> -10.
+            | "B--A---" ->  -4.
+            | "-B-A---" ->  -1.
+            | "--BA---" ->   3.
+            | _         ->   0.
         end) in
 
-        TestAI.minimax Player.A 1 (Player.A, Player.B) Board.empty |> List.of_enum |>
-        assert_equal [-1.; -3.; -10.; -4.; 0.; 0.; 0.]
+        TestAI.minimax 1 Player.A (Player.A, Player.B) Board.empty |> List.of_enum |>
+        assert_equal [-2.; -3.; -10.; -4.; 0.; 0.; 0.];
+        );
+      "choose_move will return the move with the highest score" >:: (fun() ->
+        let module TestAI = Ai.Make (struct include Board
+          let evaluate player board =
+            match board |> to_string |> Str.split (Str.regexp "\n") |> List.last with
+            | "---A---" -> 1.
+            | _         -> 0.
+        end) in
+
+        TestAI.choose_move 0 Player.A (TestGame.create Player.A Player.B) |>
+        assert_equal 4
         );
   ]) |> ignore;
