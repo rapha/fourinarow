bc3b118b0eca37fc652e0f791e7163d2dfe7bf9b
Raphael Speyer <rspeyer@gmail.com>
2010-02-25 06:38:17 +0530

    cleaned up tests a bit

--- test.ml
+++ test.ml
@@ -4,27 +4,29 @@ open OUnit
 
 let _ =
   let move _ = 3 in
+  let lines = Str.split (Str.regexp "\n") in
 
-  run_test_tt_main ("Player" >::: [
-      "to_string A" >:: (fun() ->
-        Player.A |> Player.to_string |> assert_equal "A"
-      );
-      "to_string B" >:: (fun() ->
-        Player.B |> Player.to_string |> assert_equal "B"
-      );
-  ]) |> ignore;
-  run_test_tt_main ("Board.wins" >::: [
-      "false for empty" >:: (fun() ->
+  run_test_tt_main ("unit tests" >::: [
+    ("Player" >::: [
+        "to_string A" >:: (fun() ->
+          Player.A |> Player.to_string |> assert_equal "A"
+        );
+        "to_string B" >:: (fun() ->
+          Player.B |> Player.to_string |> assert_equal "B"
+        );
+    ]);
+    ("Board.wins" >::: [
+      "is false for empty" >:: (fun() ->
         Board.empty |> Board.wins Player.A |> assert_equal false
       );
-      "false for vertical line of 3" >:: (fun() ->
+      "is false for vertical line of 3" >:: (fun() ->
         Board.build [
           "A------";
           "A------";
           "A------"]
           |> Board.wins Player.A |> assert_equal false
       );
-      "true for vertical line of 4" >:: (fun() ->
+      "is true for vertical line of 4" >:: (fun() ->
         Board.build [
           "A------";
           "A------";
@@ -32,24 +34,24 @@ let _ =
           "A------"]
         |> Board.wins Player.A |> assert_equal true
       );
-      "false for horizontal line of 3" >:: (fun() ->
+      "is false for horizontal line of 3" >:: (fun() ->
         Board.build [
           "AAA----"]
         |> Board.wins Player.A |> assert_equal false
       );
-      "true for horizontal line of 4" >:: (fun() ->
+      "is true for horizontal line of 4" >:: (fun() ->
         Board.build [
           "AAAA---"]
         |> Board.wins Player.A |> assert_equal true
       );
-      "false for NE line of 3" >:: (fun() ->
+      "is false for NE line of 3" >:: (fun() ->
         Board.build [
           "--A----";
           "-AB----";
           "ABB----"]
         |> Board.wins Player.A |> assert_equal false
       );
-      "true for NE line of 4" >:: (fun() ->
+      "is true for NE line of 4" >:: (fun() ->
         Board.build [
           "---A---";
           "--AB---";
@@ -57,14 +59,14 @@ let _ =
           "ABBB---"]
         |> Board.wins Player.A |> assert_equal true
       );
-      "false for NW line of 3" >:: (fun() ->
+      "is false for NW line of 3" >:: (fun() ->
         Board.build [
           "A------";
           "BA-----";
           "BBA----"]
         |> Board.wins Player.A |> assert_equal false
       );
-      "true for NW line of 4" >:: (fun() ->
+      "is true for NW line of 4" >:: (fun() ->
         Board.build [
           "A------";
           "BA-----";
@@ -72,7 +74,7 @@ let _ =
           "BBBA---"]
         |> Board.wins Player.A |> assert_equal true
       );
-      "false when there is a gap in the line" >:: (fun() ->
+      "is false when there is a gap in the line" >:: (fun() ->
         Board.build [
           "----B--";
           "---BB--";
@@ -81,7 +83,9 @@ let _ =
           "B-AAA--"]
         |> Board.wins Player.B |> assert_equal false
       );
-      "Board.to_string for empty board is 6 row by 7 cols of -" >:: (fun() ->
+    ]);
+    ("Board.to_string" >::: [
+      "for empty board is 6 row by 7 cols of -" >:: (fun() ->
         Board.empty |> Board.to_string |> assert_equal (
           "-------\n" ^
           "-------\n" ^
@@ -90,10 +94,10 @@ let _ =
           "-------\n" ^
           "-------\n" )
       );
-  ]) |> ignore;
+    ]);
 
-  run_test_tt_main ("game" >::: [
-    "uses argument passed in to get row to drop in" >:: (fun() ->
+  ("Game.play_turn" >::: [
+    "uses function passed in to get row to drop in" >:: (fun() ->
       let col = ref None in
       let module TestGame = Game.Make (struct include Board
         let drop _ c board =
@@ -105,56 +109,64 @@ let _ =
 
       assert_equal !col (Some 3)
     );
-    "play_turn calls drop handler" >:: (fun() ->
-      let game = TestGame.create Player.A Player.B in
-      let handled = ref false in
-      let handler = function TestGame.Drop (1,3,Player.A) -> handled := true | _ -> () in
-      let game = TestGame.handle handler game in
-      game |> TestGame.play_turn move |> ignore;
-      assert_equal !handled true
+    "calls drop handler" >:: (fun() ->
+      let drop_handled = ref false in
+      let handler = function TestGame.Drop (1,3,Player.A) -> drop_handled := true | _ -> () in
+      let game = TestGame.create Player.A Player.B |> TestGame.handle handler in
+
+      game |> TestGame.play_turn (fun _ -> 3) |> ignore;
+
+      assert_equal !drop_handled true
     );
-    "play_turn calls switch player handler" >:: (fun() ->
-      let game = TestGame.create Player.A Player.B in
-      let handled = ref false in
-      let handler = function TestGame.Switch Player.B -> handled := true | _ -> () in
-      let game = TestGame.handle handler game in
+    "calls switch player handler" >:: (fun() ->
+      let switch_handled = ref false in
+      let handler = function TestGame.Switch Player.B -> switch_handled := true | _ -> () in
+      let game = TestGame.create Player.A Player.B |> TestGame.handle handler in
+
       game |> TestGame.play_turn move |> ignore;
-      assert_equal !handled true
+
+      assert_equal !switch_handled true
     );
-    "play_turn calls win handler" >:: (fun() ->
-      let first _ = 1 and second _ = 2 in
-      let game = TestGame.create Player.A Player.B |>
-        TestGame.play_turn first |> TestGame.play_turn second |>
-        TestGame.play_turn first |> TestGame.play_turn second |>
-        TestGame.play_turn first |> TestGame.play_turn second in
+    "calls win handler" >:: (fun() ->
       let win_handled = ref false in
       let handler = function TestGame.Win Player.A -> win_handled := true | _ -> () in
-      let game = TestGame.handle handler game in
-      game |> TestGame.play_turn first |> ignore;
+      let first_col _ = 1 and second_col _ = 2 in
+      let drop_in = TestGame.play_turn in
+      let game = TestGame.create Player.A Player.B |> TestGame.handle handler |>
+        drop_in first_col |> drop_in second_col |>
+        drop_in first_col |> drop_in second_col |>
+        drop_in first_col |> drop_in second_col in
+
+      game |> drop_in first_col |> ignore;
+
       assert_equal !win_handled true
     );
-    "play_turn toggles current player" >:: (fun() ->
-      let game = TestGame.create Player.A Player.B in
-      let players = ref [] in
-      let handler = function TestGame.Switch a -> players := (a :: !players) | _ -> () in
-      game |> TestGame.handle handler |> TestGame.play_turn move |> TestGame.play_turn move |> TestGame.play_turn move |> ignore;
-      assert_equal !players [Player.B;Player.A;Player.B];
-    );
-  ]) |> ignore;
+    "toggles current player" >:: (fun() ->
+      let player_history = ref [] in
+      let handler = function TestGame.Switch a -> player_history := (a :: !player_history) | _ -> () in
+      let game = TestGame.create Player.A Player.B |> TestGame.handle handler in 
 
-  run_test_tt_main ("AI" >::: [
+      game |> TestGame.play_turn move |> TestGame.play_turn move |> TestGame.play_turn move |> ignore;
+
+      assert_equal !player_history [Player.B;Player.A;Player.B];
+    );
+  ]);
+
+  ("AI" >::: [
       "minimax returns 0 if empty" >:: (fun() ->
         let module TestAI = Ai.Make(Board) in
+
         Board.empty |> TestAI.minimax 0 Player.A (Player.A,Player.B) TestAI.winning_score |>
         assert_equal 0.
       );
-      "minimax returns -Inf if opponent has won" >:: (fun() ->
+      "minimax returns losing score if opponent has won" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
           let wins player board =
             player = Player.B
         end) in
+
         TestAI.minimax 0 Player.A (Player.A,Player.B) TestAI.winning_score Board.empty |>
-        assert_equal ~printer:string_of_float TestAI.losing_score
+        assert_equal TestAI.losing_score
       );
       "minimax with depth 0 returns value from eval function" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
@@ -164,62 +176,60 @@ let _ =
         TestAI.minimax 0 Player.A (Player.A, Player.B) TestAI.winning_score Board.empty |>
         assert_equal 5.
         );
-      "minimax with depth 1 returns Inf if column makes player win" >:: (fun() ->
+      "minimax with depth 1 returns winning score if player can win this turn" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
           let wins player board =
-            match board |> to_string |> Str.split (Str.regexp "\n") |> List.last with
-            | "----A--" -> true
-            | _         -> false
+            player = Player.A
         end) in
 
         TestAI.minimax 1 Player.A (Player.A, Player.B) TestAI.winning_score Board.empty |>
-        assert_equal ~printer:string_of_float TestAI.winning_score
+        assert_equal TestAI.winning_score
         );
-      "minimax with depth 1 returns highest values from eval function after 1 turn" >:: (fun() ->
+      "minimax with depth 1 returns highest values from eval function after this turn" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
           let evaluate player board =
-            match board |> to_string |> Str.split (Str.regexp "\n") |> List.last with
+            match board |> to_string |> lines |> List.last with
             | "-A-----" ->  5.
             | _         ->  0.
         end) in
 
         TestAI.minimax 1 Player.A (Player.A, Player.B) TestAI.winning_score Board.empty |>
-        assert_equal ~printer:string_of_float 5.
+        assert_equal 5.
         );
       "minimax with depth 2 returns highest of lowest eval values after 2 turns" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
           let evaluate player board =
-            let bottom_row = board |> to_string |> Str.split (Str.regexp "\n") |> List.last in
-            let col_with_A = Str.search_forward (Str.regexp "A") bottom_row 0 |> (+) 1 in
-            col_with_A |> ( * ) (-1) |> float_of_int
+            let bottom_row = board |> to_string |> lines |> List.last in
+            let col_with player = 
+              try Str.search_forward (Str.regexp (player |> Player.to_string)) bottom_row 0 |> (+) 1
+              with Not_found -> 0
+            in
+            (col_with Player.A) + (col_with Player.B) |> ( * ) (-1) |> float_of_int
         end) in
 
         TestAI.minimax 2 Player.A (Player.A, Player.B) TestAI.winning_score Board.empty |>
-        assert_equal ~printer:string_of_float (-1.)
+        assert_equal (-8.)
         );
-      "minimax returns an -Inf value for a full column" >:: (fun() ->
-        let module TestAI = Ai.Make (Board) in
+      "minimax with depth 1 returns an full score value for a full column" >:: (fun() ->
+        let module TestAI = Ai.Make (struct include Board
+          let drop _ _ _ = 
+            failwith "column full"
+        end) in
 
-        TestAI.minimax 1 Player.A (Player.A, Player.B) TestAI.winning_score (Board.build [
-              "BABBBAB";
-              "ABAAABA";
-              "BABBBAB";
-              "ABAAABA";
-              "BABBBAB";
-              "ABAAABA";
-            ]) |>
-        assert_equal TestAI.losing_score
+        TestAI.minimax 1 Player.A (Player.A, Player.B) TestAI.full_score Board.empty |>
+        assert_equal TestAI.full_score
         );
       "choose_column will return the move with the highest score" >:: (fun() ->
         let module TestAI = Ai.Make (struct include Board
-          let evaluate player board =
-            match board |> to_string |> Str.split (Str.regexp "\n") |> List.last with
+          let evaluate _ board =
+            match board |> to_string |> lines |> List.last with
             | "---A---" -> 1.
             | _         -> 0.
         end) in
 
         TestAI.choose_column 0 (TestGame.create Player.A Player.B) |>
-        assert_equal 3
+        assert_equal ~printer:string_of_int 3
         );
+    ]);
   ]) |> ignore;
 
