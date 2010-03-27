717c99cd96c5690bc22c021c941c3bb70aa1f30a
Raphael Speyer <rspeyer@gmail.com>
2010-03-27 12:28:24 +1100

    provided interface for board module to hide functions we do not want to share

---
+++ board.mli
@@ -0,0 +1,9 @@
+exception Column_full of int
+type t
+val empty : t
+val drop : Piece.t -> int -> t -> t
+val wins : Piece.t -> t -> bool
+val top_row : int -> t -> int
+val to_string : t -> string
+val build : string list -> t
+val evaluate : Piece.t -> t -> float
