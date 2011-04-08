966308ae09b58194c24fd62d2470e36eabb9ef53
Raphael Speyer <rspeyer@gmail.com>
2011-04-09 08:56:33 +1000

    some tidying of tui.ml

--- tui.ml
+++ tui.ml
@@ -3,7 +3,6 @@ open Printf
 module TuiAi = Ai.Make(Board)
 module TuiGame = Game.Make(Board)
 
-let winner = ref None
 let view = [|
   [|'-';'-';'-';'-';'-';'-';'-';|];
   [|'-';'-';'-';'-';'-';'-';'-';|];
@@ -12,7 +11,6 @@ let view = [|
   [|'-';'-';'-';'-';'-';'-';'-';|];
   [|'-';'-';'-';'-';'-';'-';'-';|];
 |]
-
 let row_heights = [|0;0;0;0;0;0;0|]
 
 let depth =
@@ -23,29 +21,32 @@ let prompt_column _ _ =
   printf "column: "; flush stdout;
   input_line stdin |> int_of_string |> flip (-) 1 |> Col.of_int
 
-let print game =
-  let str = Array.fold_left (fun sofar cells ->
-    sofar ^ (Array.fold_left (fun s c -> s ^ " " ^ string_of_char c) "" cells) ^ "\n"
-  ) "" view 
-  in
-  printf "%s\n" str; 
-  flush stdout;
-  game
+let print () =
+  let join separator = Array.fold_left (flip (^) separator |- (^)) "" in
+
+  view 
+    |> Array.map (Array.map string_of_char |- join " ") 
+    |> join "\n"
+    |> print_endline;
+  flush stdout
 
 let rec loop game =
-  match !winner with
-  | Some p -> p |> Piece.to_string |> printf "%s has won\n"
-  | None -> game |> TuiGame.play_turn |> print |> loop
+  game |> TuiGame.play_turn |> loop
 
 let win_handler piece = 
-  winner := Some piece
+  piece |> Piece.to_string |> printf "%s has won\n"; 
+  exit 0
 
 let drop_handler (col, piece) =
   let coli = col |> Col.to_int in
   let rowi = row_heights.(coli) in
   view.(5 - rowi).(coli) <- Piece.to_char piece;
-  row_heights.(coli) <- rowi + 1
+  row_heights.(coli) <- rowi + 1;
+  print ()
 
 let _ =
   let players = Player.create_pair (prompt_column, TuiAi.choose_column depth) in
-  TuiGame.create players |> TuiGame.on_drop drop_handler |> TuiGame.on_win (fun piece -> winner := Some piece) |> loop
+  TuiGame.create players 
+    |> TuiGame.on_drop drop_handler 
+    |> TuiGame.on_win win_handler
+    |> loop
